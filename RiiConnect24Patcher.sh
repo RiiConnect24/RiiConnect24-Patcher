93354639610bec7e5b1dc5c625c00abd7fcf921d
Harrison <53527582+HTV04@users.noreply.github.com>
2021-01-30 15:03:21 -0500

    printf -> print

--- RiiConnect24Patcher.sh
+++ RiiConnect24Patcher.sh
@@ -10,13 +10,13 @@ print () {
 
 # Print string and wait for input to store in variable
 input () {
-	printf "${1}" | fold -s -w $(tput cols)
+	print "${1}" | fold -s -w $(tput cols)
 	read -n -r ${2}
 }
 
 # Prints "Press any key to " + string given, and then a period
 anykey() {
-	printf "Press any key to ${1}." | fold -s -w $(tput cols)
+	print "Press any key to ${1}." | fold -s -w $(tput cols)
 	read -n 1 -r
 }
 
